57666408aa34922f29f3e0f6c047b3aef1d5e578
Wei Li <wei@chromium.org>
2024-10-09 11:07:41 -0700

    Format for readability

--- src/pages/home/_FeatureHero.svelte
+++ src/pages/home/_FeatureHero.svelte
@@ -22,37 +22,49 @@
           ></iframe>
         </div>
       </DeviceMockup>
-    </div>  
+    </div>
     <HeroHeader
       h1Class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight !leading-[1.05] md:text-5xl xl:text-6xl dark:text-white"
       pClass="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-200"
     >
       <svelte:fragment slot="h1">
-        Me In a 
-          <mark
-            class="my-1 rounded bg-primary-50 px-2 py-0 text-white dark:bg-primary-700"
-            >Nutshell</mark
-          >
+        Me In a
+        <mark
+          class="my-1 rounded bg-primary-50 px-2 py-0 text-white dark:bg-primary-700"
+          >Nutshell</mark
+        >
         🌰
       </svelte:fragment>
       <svelte:fragment slot="paragraph">
-              <div class="block sm:hidden">
-
+        <div class="block sm:hidden">
           <span class="font-bold text-primary-200">Wei Li</span>
-          I am currently working at Microsft as a Sr Principal Applied Scientist. I was staff research scientist and manager at NewsBreak (series C startup). I was a senior research scientist at Facebook Seattle working on Video Ads and Marketplace Product Intelligence. Before Facebook, I was a senior software engineer at Google Research in Zurich, Switzerland. My research at Google focused on video analysis and its applications in making YouTube free and enjoyable. I also work with academia through university relationship. I worked closely with ETH CVL led by Professor Luc Van Gool on co-organizing Webvision challenge and workshop.
+          I am currently working at Microsft as a Sr Principal Applied Scientist.
+          I was staff research scientist and manager at NewsBreak (series C startup).
+          I was a senior research scientist at Facebook Seattle working on Video
+          Ads and Marketplace Product Intelligence. Before Facebook, I was a senior
+          software engineer at Google Research in Zurich, Switzerland. My research
+          at Google focused on video analysis and its applications in making YouTube
+          free and enjoyable. I also work with academia through university relationship.
+          I worked closely with ETH CVL led by Professor Luc Van Gool on co-organizing
+          Webvision challenge and workshop.
 
-<br />
-I got my MPhil from The Chinese University of Hong Kong supervised by Professor Xiaogang Wang. I was also a member of MMlab and worked closely with Professor Xiaoou Tang. Before CUHK, I got my bachelor from Tsinghua University, where I was a member of the Yao Class. I did my bachelor thesis with Professor Changshui Zhang.
+          <br />
+          I got my MPhil from The Chinese University of Hong Kong supervised by Professor
+          Xiaogang Wang. I was also a member of MMlab and worked closely with Professor
+          Xiaoou Tang. Before CUHK, I got my bachelor from Tsinghua University, where
+          I was a member of the Yao Class. I did my bachelor thesis with Professor
+          Changshui Zhang.
 
-<br />
-After my daytime research, I am a contributor to scikit-learn. A Kaggle Master. Reviewer for computer vision journals and conferences. A history lover, an ameteur photographer and a traveler (30+ countries and counting).          
-</div>
+          <br />
+          After my daytime research, I am a contributor to scikit-learn. A Kaggle
+          Master. Reviewer for computer vision journals and conferences. A history
+          lover, an ameteur photographer and a traveler (30+ countries and counting).
+        </div>
       </svelte:fragment>
     </HeroHeader>
 
     <div
       class="md:text-md lg:text-md mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-200 lg:mb-8"
-    >
-    </div>
+    ></div>
   </div>
 </Section>
